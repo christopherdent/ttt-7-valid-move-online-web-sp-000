1f841d1f8942700bd412d89277d7d3f82d2f92c4
Christopher Dent <christopherdent01@gmail.com>
2019-06-29 00:51:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,12 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0, 8)
+  
+  end
+
+end 
+
 def position_taken?(board, index)
   if board[0] == "X"
     true 
