d6825d4f2ac276eb52b2600c6bd20f1f4353629f
Christopher Dent <christopherdent01@gmail.com>
2019-06-29 00:36:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
   
-  if !position_taken?(board, index) && index.between(0, 8)
+  if !position_taken?(board, index) && index.between?(0, 8)
   end
 
 end 
