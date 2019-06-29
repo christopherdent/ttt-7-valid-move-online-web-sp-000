2a9072d6048a76e94aa5e1827515bcc5a624e874
Christopher Dent <christopherdent01@gmail.com>
2019-06-29 00:22:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
+def valid_move?(board, index)
+  
+  if position_taken?(board, index)
+    false 
+  elsif index.between(0, 8)
+    true
+  else
+    false
+  end
 
+end 
 
 
 def position_taken?(board, index)
